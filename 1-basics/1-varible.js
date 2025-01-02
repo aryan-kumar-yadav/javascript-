e5dcb49c6952072cf2b8a5be77befe1500b0b5ba
aryan-kumar-yadav <eaglerise19@gmail.com>
2025-01-02 16:24:15 +0530

    1-varible.js

--- 1-basics/1-varible.js
+++ 1-basics/1-varible.js
@@ -4,16 +4,16 @@ console.log("aryan")
 console.log("aditya")
 console.log("riya")
 console.log("rahaual")
-/* here console.log()is used to show as a output written in bracket*/
-const accountId =7999
+/* here console.log()is used to show as a output written in bracket in the terminal */
+const accountId =7999 //think that should be keep constant through out the code
 let accountEmail ="shishing75@gmail.com"
 var accountPassword =" shishing"
 accountCity = "delhi"
 //js is a safe that their is possibility of storing without using any variable 
 let accountState
-// varible with no valuewill be considered undefined 
+// varible with no value will be considered undefined 
 console.log(accountId);
-//prefer nort use var instead use let 
+//prefer not use var instead use let 
 //because of the issue in block scope and function scope 
 
 accountEmail ="shi@m.com"
@@ -22,3 +22,5 @@ accountCity ="kanpur"
 console.log();//log process to write the email password and city
 //instead use console table as shown below for writing multiple output
 console.table([accountEmail,accountId,accountPassword,accountCity ,accountState])
+// output is in the form of tables
+
